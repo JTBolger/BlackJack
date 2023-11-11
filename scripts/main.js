7a687473507ccf2c47faf4c98b12b6947402a8c0
JTBolger <112192375+JTBolger@users.noreply.github.com>
2023-11-10 20:25:45 -0600

    first deal completed
    completed first deal. a place to add the values is in the middle

--- scripts/main.js
+++ scripts/main.js
@@ -103,18 +103,30 @@ var pCard9 = document.getElementById("pCard9")
 
 var cardFace = document.getElementById("card")
 
+var dealerValue = 0
+var playerValue = 0
 function drawCard(dealingCard) {
+    var tempValue = 0
     if (dealingCard == 1) {
         dCard1.style.display = "block"
         dCard1 = randomCard(deck)
         console.log("dCard1 = "+dCard1)
         document.getElementById("dCard1-front").innerHTML = dCard1
+        tempValue = Value(dCard1)
+        dealerValue = dealerValue + tempValue
+        console.log("dealerValue = "+dealerValue)
+        document.getElementById("dealer-value").innerHTML = dealerValue
+
     }
     else if (dealingCard == 2) {
         dCard2.style.display = "block"
         dCard2 = randomCard(deck)
         console.log("dCard2 = "+dCard2)
         document.getElementById("dCard2-front").innerHTML = "<img src=\"images/PlayingCardBack.png\" style=\"width: 100%; height: 100%; border-radius: 10px;\">"
+        tempValue = Value(dCard2)
+        dealerValue = dealerValue + tempValue
+        console.log("dealerValue = "+dealerValue)
+        // document.getElementById("dealer-value").innerHTML = "??"
     }
     else if (dealingCard == 3) {
         dCard3.style.display = "block"
@@ -122,96 +134,160 @@ function drawCard(dealingCard) {
         console.log("dCard3 = "+dCard3)
         document.getElementById("dCard3-front").innerHTML = dCard3
         dCard3.style.display = "block"
+        tempValue = Value(dCard3)
+        dealerValue = dealerValue + tempValue
+        console.log("dealerValue = "+dealerValue)
+        document.getElementById("dealer-value").innerHTML = dealerValue
     }
     else if (dealingCard == 4) {
         dCard4.style.display = "block"
         dCard4 = randomCard(deck)
         console.log("dCard4 = "+dCard4)
         document.getElementById("dCard4-front").innerHTML = dCard4
+        tempValue = Value(dCard4)
+        dealerValue = dealerValue + tempValue
+        console.log("dealerValue = "+dealerValue)
+        document.getElementById("dealer-value").innerHTML = dealerValue
     }
     else if (dealingCard == 5) {
         dCard5.style.display = "block"
         dCard5 = randomCard(deck)
         console.log("dCard5 = "+dCard5)
         document.getElementById("dCard5-front").innerHTML = dCard5
+        tempValue = Value(dCard5)
+        dealerValue = dealerValue + tempValue
+        console.log("dealerValue = "+dealerValue)
+        document.getElementById("dealer-value").innerHTML = dealerValue
     }
     else if (dealingCard == 6) {
         dCard6.style.display = "block"
         dCard6 = randomCard(deck)
         console.log("dCard6 = "+dCard6)
         document.getElementById("dCard6-front").innerHTML = dCard6
+        tempValue = Value(dCard6)
+        dealerValue = dealerValue + tempValue
+        console.log("dealerValue = "+dealerValue)
+        document.getElementById("dealer-value").innerHTML = dealerValue
     }
     else if (dealingCard == 7) {
         dCard7.style.display = "block"
         dCard7 = randomCard(deck)
         console.log("dCard7 = "+dCard7)
         document.getElementById("dCard7-front").innerHTML = dCard7
+        tempValue = Value(dCard7)
+        dealerValue = dealerValue + tempValue
+        console.log("dealerValue = "+dealerValue)
+        document.getElementById("dealer-value").innerHTML = dealerValue
     }
     else if (dealingCard == 8) {
         dCard8.style.display = "block"
         dCard8 = randomCard(deck)
         console.log("dCard8 = "+dCard8)
         document.getElementById("dCard8-front").innerHTML = dCard8
+        tempValue = Value(dCard8)
+        dealerValue = dealerValue + tempValue
+        console.log("dealerValue = "+dealerValue)
+        document.getElementById("dealer-value").innerHTML = dealerValue
     }
     else if (dealingCard == 9) {
         dCard9.style.display = "block"
         dCard9 = randomCard(deck)
         console.log("dCard9 = "+dCard9)
         document.getElementById("dCard9-front").innerHTML = dCard9
+        tempValue = Value(dCard9)
+        dealerValue = dealerValue + tempValue
+        console.log("dealerValue = "+dealerValue)
+        document.getElementById("dealer-value").innerHTML = dealerValue
     }
     else if (dealingCard == 10) {
         pCard1.style.display = "block"
         pCard1 = randomCard(deck)
         console.log("pCard1 = "+pCard1)
         document.getElementById("pCard1-front").innerHTML = pCard1
+        tempValue = Value(pCard1)
+        playerValue = tempValue
+        console.log("playerValue = "+playerValue)
+        document.getElementById("player-value").innerHTML = playerValue
     }
     else if (dealingCard == 11) {
         pCard2.style.display = "block"
         pCard2 = randomCard(deck)
         console.log("pCard2 = "+pCard2)
         document.getElementById("pCard2-front").innerHTML = pCard2
+        tempValue = Value(pCard2)
+        playerValue = playerValue + tempValue
+        console.log("playerValue = "+playerValue)
+        document.getElementById("player-value").innerHTML = playerValue
     }
     else if (dealingCard == 12) {
         pCard3.style.display = "block"
         pCard3 = randomCard(deck)
         console.log("pCard3 = "+pCard3)
         document.getElementById("pCard3-front").innerHTML = pCard3
+        tempValue = Value(pCard3)
+        player = player + tempValue
+        console.log("player = "+player)
+        document.getElementById("player-value").innerHTML = playerValue
     }
     else if (dealingCard == 13) {
         pCard4.style.display = "block"
         pCard4 = randomCard(deck)
         console.log("pCard4 = "+pCard4)
         document.getElementById("pCard4-front").innerHTML = pCard4
+        tempValue = Value(pCard4)
+        player = player + tempValue
+        console.log("player = "+player)
+        document.getElementById("player-value").innerHTML = playerValue
     }
     else if (dealingCard == 14) {
         pCard5.style.display = "block"
         pCard5 = randomCard(deck)
         console.log("pCard5 = "+pCard5)
         document.getElementById("pCard5-front").innerHTML = pCard5
+        tempValue = Value(pCard5)
+        player = player + tempValue
+        console.log("player = "+player)
+        document.getElementById("player-value").innerHTML = playerValue
     }
     else if (dealingCard == 15) {
         pCard6.style.display = "block"
         pCard6 = randomCard(deck)
         console.log("pCard6 = "+pCard6)
         document.getElementById("pCard6-front").innerHTML = pCard6
+        tempValue = Value(pCard6)
+        player = player + tempValue
+        console.log("player = "+player)
+        document.getElementById("player-value").innerHTML = playerValue
     }
     else if (dealingCard == 16) {
         pCard7.style.display = "block"
         pCard7 = randomCard(deck)
         console.log("pCard7 = "+pCard7)
         document.getElementById("pCard7-front").innerHTML = pCard7
+        tempValue = Value(pCard7)
+        player = player + tempValue
+        console.log("player = "+player)
+        document.getElementById("player-value").innerHTML = playerValue
     }
     else if (dealingCard == 17) {
         pCard8.style.display = "block"
         pCard8 = randomCard(deck)
         console.log("pCard8 = "+pCard8)
         document.getElementById("pCard8-front").innerHTML = pCard8
+        tempValue = Value(pCard8)
+        player = player + tempValue
+        console.log("player = "+player)
+        document.getElementById("player-value").innerHTML = playerValue
     }
     else if (dealingCard == 18) {
         pCard9.style.display = "block"
         pCard9 = randomCard(deck)
         console.log("pCard9 = "+pCard9)
         document.getElementById("pCard9-front").innerHTML = pCard9
+        tempValue = Value(pCard9)
+        player = player + tempValue
+        console.log("player = "+player)
+        document.getElementById("player-value").innerHTML = playerValue
     }
 }
 
@@ -220,4 +296,176 @@ function beginDeal() {
     drawCard(2)
     drawCard(10)
     drawCard(11)
+}
+
+
+function Value(card) {
+    if (card == "2C") {
+        return 2
+    }
+    else if (card == "3C") {
+        return 3
+    }
+    else if (card == "4C") {
+        return 4
+    }
+    else if (card == "5C") {
+        return 5
+    }
+    else if (card == "6C") {
+        return 6
+    }
+    else if (card == "7C") {
+        return 7
+    }
+    else if (card == "8C") {
+        return 8
+    }
+    else if (card == "9C") {
+        return 9
+    }
+    else if (card == "10C") {
+        return 10
+    }
+    else if (card == "JC") {
+        return 10
+    }
+    else if (card == "QC") {
+        return 10
+    }
+    else if (card == "KC") {
+        return 10
+    }
+    else if (card == "2D") {
+        return 2
+    }
+    else if (card == "3D") {
+        return 3
+    }
+    else if (card == "4D") {
+        return 4
+    }
+    else if (card == "5D") {
+        return 5
+    }
+    else if (card == "6D") {
+        return 6
+    }
+    else if (card == "7D") {
+        return 7
+    }
+    else if (card == "8D") {
+        return 8
+    }
+    else if (card == "9D") {
+        return 9
+    }
+    else if (card == "1D") {
+        return 10
+    }
+    else if (card == "JD") {
+        return 10
+    }
+    else if (card == "QD") {
+        return 10
+    }
+    else if (card == "KD") {
+        return 10
+    }
+    else if (card == "2H") {
+        return 2
+    }
+    else if (card == "3H") {
+        return 3
+    }
+    else if (card == "4H") {
+        return 4
+    }
+    else if (card == "5H") {
+        return 5
+    }
+    else if (card == "6H") {
+        return 6
+    }
+    else if (card == "7H") {
+        return 7
+    }
+    else if (card == "8H") {
+        return 8
+    }
+    else if (card == "9H") {
+        return 9
+    }
+    else if (card == "10H") {
+        return 10
+    }
+    else if (card == "JH") {
+        return 10
+    }
+    else if (card == "QH") {
+        return 10
+    }
+    else if (card == "KH") {
+        return 10
+    }
+    else if (card == "2S") {
+        return 2
+    }
+    else if (card == "3S") {
+        return 3
+    }
+    else if (card == "4S") {
+        return 4
+    }
+    else if (card == "5S") {
+        return 5
+    }
+    else if (card == "6S") {
+        return 6
+    }
+    else if (card == "7S") {
+        return 7
+    }
+    else if (card == "8S") {
+        return 8
+    }
+    else if (card == "9S") {
+        return 9
+    }
+    else if (card == "10S") {
+        return 10
+    }
+    else if (card == "JS") {
+        return 10
+    }
+    else if (card == "QS") {
+        return 10
+    }
+    else if (card == "KS") {
+        return 10
+    }
+    else if (card == "AC" && (dealerValue < 11 || playerValue < 11)) {
+        return 11
+    }
+    else if (card == "AC" && (dealerValue >= 11 || playerValue >= 11)) {
+        return 1
+    }
+    else if (card == "AD" && (dealerValue < 11 || playerValue < 11)) {
+        return 11
+    }
+    else if (card == "AD" && (dealerValue >= 11 || playerValue >= 11)) {
+        return 1
+    }
+    else if (card == "AH" && (dealerValue < 11 || playerValue < 11)) {
+        return 11
+    }
+    else if (card == "AH" && (dealerValue >= 11 || playerValue >= 11)) {
+        return 1
+    }
+    else if (card == "AS" && (dealerValue < 11 || playerValue < 11)) {
+        return 11
+    }
+    else if (card == "AS" && (dealerValue >= 11 || playerValue >= 11)) {
+        return 1
+    }
 }
