5867a1ec730aa7a982aed687081cfc34bb959fe8
JTBolger <112192375+JTBolger@users.noreply.github.com>
2023-11-10 23:39:07 -0600

    calculator fixed
    fixed the calculator for the cards so aces are now worth 1 or 11

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,8 @@ var betDisplay = document.getElementById("bet")
 var incBet = document.getElementById("add-bet")
 var decBet = document.getElementById("subtract-bet")
 var finBet = document.getElementById("confirm-bet")
+var hitButton = document.getElementById("hit-button")
+var stayButton = document.getElementById("stay-button")
 
 var bank = 100
 var bet = 0
@@ -103,6 +105,7 @@ var pCard9 = document.getElementById("pCard9")
 
 var cardFace = document.getElementById("card")
 
+var hitNumber = 0
 var dealerValue = 0
 var playerValue = 0
 function drawCard(dealingCard) {
@@ -204,97 +207,90 @@ function drawCard(dealingCard) {
         pCard1 = randomCard(deck)
         console.log("pCard1 = "+pCard1)
         document.getElementById("pCard1-front").innerHTML = pCard1
-        tempValue = Value(pCard1)
-        playerValue = tempValue
-        console.log("playerValue = "+playerValue)
-        document.getElementById("player-value").innerHTML = playerValue
+        calculatePlayerValue(pCard1)
+        hitNumber = 1
     }
     else if (dealingCard == 11) {
         pCard2.style.display = "block"
         pCard2 = randomCard(deck)
         console.log("pCard2 = "+pCard2)
         document.getElementById("pCard2-front").innerHTML = pCard2
-        tempValue = Value(pCard2)
-        playerValue = playerValue + tempValue
-        console.log("playerValue = "+playerValue)
-        document.getElementById("player-value").innerHTML = playerValue
+        calculatePlayerValue(pCard2)
+        hitNumber = 2
     }
     else if (dealingCard == 12) {
         pCard3.style.display = "block"
         pCard3 = randomCard(deck)
         console.log("pCard3 = "+pCard3)
         document.getElementById("pCard3-front").innerHTML = pCard3
-        tempValue = Value(pCard3)
-        player = player + tempValue
-        console.log("player = "+player)
-        document.getElementById("player-value").innerHTML = playerValue
+        calculatePlayerValue(pCard3)
+        hitNumber = 3
     }
     else if (dealingCard == 13) {
         pCard4.style.display = "block"
         pCard4 = randomCard(deck)
         console.log("pCard4 = "+pCard4)
         document.getElementById("pCard4-front").innerHTML = pCard4
-        tempValue = Value(pCard4)
-        player = player + tempValue
-        console.log("player = "+player)
-        document.getElementById("player-value").innerHTML = playerValue
+        calculatePlayerValue(pCard4)
+        hitNumber = 4
     }
     else if (dealingCard == 14) {
         pCard5.style.display = "block"
         pCard5 = randomCard(deck)
         console.log("pCard5 = "+pCard5)
         document.getElementById("pCard5-front").innerHTML = pCard5
-        tempValue = Value(pCard5)
-        player = player + tempValue
-        console.log("player = "+player)
-        document.getElementById("player-value").innerHTML = playerValue
+        calculatePlayerValue(pCard5)
+        hitNumber = 5
     }
     else if (dealingCard == 15) {
         pCard6.style.display = "block"
         pCard6 = randomCard(deck)
         console.log("pCard6 = "+pCard6)
         document.getElementById("pCard6-front").innerHTML = pCard6
-        tempValue = Value(pCard6)
-        player = player + tempValue
-        console.log("player = "+player)
-        document.getElementById("player-value").innerHTML = playerValue
+        calculatePlayerValue(pCard6)
+        hitNumber = 6
     }
     else if (dealingCard == 16) {
         pCard7.style.display = "block"
         pCard7 = randomCard(deck)
         console.log("pCard7 = "+pCard7)
         document.getElementById("pCard7-front").innerHTML = pCard7
-        tempValue = Value(pCard7)
-        player = player + tempValue
-        console.log("player = "+player)
-        document.getElementById("player-value").innerHTML = playerValue
+        calculatePlayerValue(pCard7)
+        hitNumber = 7
     }
     else if (dealingCard == 17) {
         pCard8.style.display = "block"
         pCard8 = randomCard(deck)
         console.log("pCard8 = "+pCard8)
         document.getElementById("pCard8-front").innerHTML = pCard8
-        tempValue = Value(pCard8)
-        player = player + tempValue
-        console.log("player = "+player)
-        document.getElementById("player-value").innerHTML = playerValue
+        calculatePlayerValue(pCard8)
+        hitNumber = 8
     }
     else if (dealingCard == 18) {
         pCard9.style.display = "block"
         pCard9 = randomCard(deck)
         console.log("pCard9 = "+pCard9)
         document.getElementById("pCard9-front").innerHTML = pCard9
-        tempValue = Value(pCard9)
-        player = player + tempValue
-        console.log("player = "+player)
-        document.getElementById("player-value").innerHTML = playerValue
+        calculatePlayerValue(pCard9)
+        hitNumber = 9
     }
 }
 
 function beginDeal() {
+    hitButton.style.display = "flex"
+    stayButton.style.display = "flex"
     drawCard(1)
+    setTimeout(drawCardTwo, 500)
+    setTimeout(drawCardTen, 1000)
+    setTimeout(drawCardEleven, 1500)
+}
+function drawCardTwo() {
     drawCard(2)
+}
+function drawCardTen() {
     drawCard(10)
+}
+function drawCardEleven() {
     drawCard(11)
 }
 
@@ -360,7 +356,7 @@ function Value(card) {
     else if (card == "9D") {
         return 9
     }
-    else if (card == "1D") {
+    else if (card == "10D") {
         return 10
     }
     else if (card == "JD") {
@@ -444,28 +440,289 @@ function Value(card) {
     else if (card == "KS") {
         return 10
     }
-    else if (card == "AC" && (dealerValue < 11 || playerValue < 11)) {
-        return 11
+    // else if (card == "AC" && (dealerValue < 11 || playerValue < 11)) {
+    //     return 11
+    // }
+    // else if (card == "AC" && (dealerValue >= 11 || playerValue >= 11)) {
+    //     return 1
+    // }
+    // else if (card == "AD" && (dealerValue < 11 || playerValue < 11)) {
+    //     return 11
+    // }
+    // else if (card == "AD" && (dealerValue >= 11 || playerValue >= 11)) {
+    //     return 1
+    // }
+    // else if (card == "AH" && (dealerValue < 11 || playerValue < 11)) {
+    //     return 11
+    // }
+    // else if (card == "AH" && (dealerValue >= 11 || playerValue >= 11)) {
+    //     return 1
+    // }
+    // else if (card == "AS" && (dealerValue < 11 || playerValue < 11)) {
+    //     return 11
+    // }
+    // else if (card == "AS" && (dealerValue >= 11 || playerValue >= 11)) {
+    //     return 1
+    // }
+}
+
+function HIT() {
+    if (playerValue < 21) {
+        drawCard((hitNumber + 10))
     }
-    else if (card == "AC" && (dealerValue >= 11 || playerValue >= 11)) {
-        return 1
-    }
-    else if (card == "AD" && (dealerValue < 11 || playerValue < 11)) {
-        return 11
-    }
-    else if (card == "AD" && (dealerValue >= 11 || playerValue >= 11)) {
-        return 1
-    }
-    else if (card == "AH" && (dealerValue < 11 || playerValue < 11)) {
-        return 11
-    }
-    else if (card == "AH" && (dealerValue >= 11 || playerValue >= 11)) {
-        return 1
-    }
-    else if (card == "AS" && (dealerValue < 11 || playerValue < 11)) {
-        return 11
-    }
-    else if (card == "AS" && (dealerValue >= 11 || playerValue >= 11)) {
-        return 1
+    else {
+        console.error("player has busted/reached 21")
     }
 }
+
+// function calculatePlayerValue(cardForCalculation) {
+//     if ((pCard1 == "AH" || pCard2 == "AH" || pCard3 == "AH" || pCard4 == "AH" || pCard5 == "AH" || pCard6 == "AH" || pCard7 == "AH" || pCard8 == "AH" || pCard9 == "AH" || pCard1 == "AC" || pCard2 == "AC" || pCard3 == "AC" || pCard4 == "AC" || pCard5 == "AC" || pCard6 == "AC" || pCard7 == "AC" || pCard8 == "AC" || pCard9 == "AC" || pCard1 == "AD" || pCard2 == "AD" || pCard3 == "AD" || pCard4 == "AD" || pCard5 == "AD" || pCard6 == "AD" || pCard7 == "AD" || pCard8 == "AD" || pCard9 == "AD" || pCard1 == "AS" || pCard2 == "AS" || pCard3 == "AS" || pCard4 == "AS" || pCard5 == "AS" || pCard6 == "AS" || pCard7 == "AS" || pCard8 == "AS" || pCard9 == "AS") ) {
+//         if ((playerValue + Value(cardForCalculation))> 21) {
+//             playerValue = playerValue - 9
+//         }
+//         else {
+//             tempValue = Value(cardForCalculation)
+//             playerValue = playerValue + tempValue
+//             console.log("playerValue = "+playerValue)
+//             document.getElementById("player-value").innerHTML = playerValue
+//         }
+//     }
+//     else {
+//         tempValue = Value(cardForCalculation)
+//         playerValue = playerValue + tempValue
+//         console.log("playerValue = "+playerValue)
+//         document.getElementById("player-value").innerHTML = playerValue
+//     }
+// }
+
+var pCard1_V = 0
+var pCard2_V = 0
+var pCard3_V = 0
+var pCard4_V = 0
+var pCard5_V = 0
+var pCard6_V = 0
+var pCard7_V = 0
+var pCard8_V = 0
+var pCard9_V = 0
+
+var dCard1_V = 0
+var dCard2_V = 0
+var dCard3_V = 0
+var dCard4_V = 0
+var dCard5_V = 0
+var dCard6_V = 0
+var dCard7_V = 0
+var dCard8_V = 0
+var dCard9_V = 0
+
+function calculatePlayerValue(CFC) {
+    tempValue = Value(CFC)
+    if ((pCard1 == "AH" || pCard1 == "AD" || pCard1 == "AC" || pCard1 == "AS")) {
+        if ((playerValue + 11) > 21) {
+            pCard2_V = 1
+        }
+        else {
+            pCard2_V = 11
+        }
+    }
+    else {
+        pCard1_V = Value(pCard1)
+    }
+    if ((pCard2 == "AH" || pCard2 == "AD" || pCard2 == "AC" || pCard2 == "AS")) {
+        if ((playerValue + 11) > 21) {
+            pCard2_V = 1
+        }
+        else {
+            pCard2_V = 11
+        }
+    }
+    else if ((hitNumber + 10) == 11) {
+        pCard2_V = Value(pCard2)
+    }
+    if ((pCard3 == "AH" || pCard3 == "AD" || pCard3 == "AC" || pCard3 == "AS")) {
+        if ((playerValue + 11) > 21) {
+            pCard3_V = 1
+        }
+        else {
+            pCard3_V = 11
+        }
+    }
+    else if ((hitNumber + 10) == 12) {
+        pCard3_V = Value(pCard3)
+    }
+    if ((pCard4 == "AH" || pCard4 == "AD" || pCard4 == "AC" || pCard4 == "AS")) {
+        if ((playerValue + 11) > 21) {
+            pCard4_V = 1
+        }
+        else {
+            pCard4_V = 11
+        }
+    }
+    else if ((hitNumber + 10) == 13) {
+        pCard4_V = Value(pCard4)
+    }
+    if ((pCard5 == "AH" || pCard5 == "AD" || pCard5 == "AC" || pCard5 == "AS")) {
+        if ((playerValue + 11) > 21) {
+            pCard5_V = 1
+        }
+        else {
+            pCard5_V = 11
+        }
+    }
+    else if ((hitNumber + 10) == 14) {
+        pCard5_V = Value(pCard5)
+    }
+    if ((pCard6 == "AH" || pCard6 == "AD" || pCard6 == "AC" || pCard6 == "AS")) {
+        if ((playerValue + 11) > 21) {
+            pCard6_V = 1
+        }
+        else {
+            pCard6_V = 11
+        }
+    }
+    else if ((hitNumber + 10) == 15) {
+        pCard6_V = Value(pCard6)
+    }
+    if ((pCard7 == "AH" || pCard7 == "AD" || pCard7 == "AC" || pCard7 == "AS")) {
+        if ((playerValue + 11) > 21) {
+            pCard2_7 = 1
+        }
+        else {
+            pCard2_7 = 11
+        }
+    }
+    else if ((hitNumber + 10) == 16) {
+        pCard7_V = Value(pCard7)
+    }
+    if ((pCard8 == "AH" || pCard8 == "AD" || pCard8 == "AC" || pCard8 == "AS")) {
+        if ((playerValue + 11) > 21) {
+            pCard8_V = 1
+        }
+        else {
+            pCard8_V = 11
+        }
+    }
+    else if ((hitNumber + 10) == 17) {
+        pCard8_V = Value(pCard8)
+    }
+    if ((pCard9 == "AH" || pCard9 == "AD" || pCard9 == "AC" || pCard9 == "AS")) {
+        if ((playerValue + 11) > 21) {
+            pCard9_V = 1
+        }
+        else {
+            pCard9_V = 11
+        }
+    }
+    else if ((hitNumber + 10) == 18) {
+        pCard9_V = Value(pCard9)
+    }
+    playerValue = pCard1_V + pCard2_V + pCard3_V + pCard4_V + pCard5_V + pCard6_V + pCard7_V + pCard8_V + pCard9_V
+    console.log("playerValue = "+playerValue)
+    document.getElementById("player-value").innerHTML = playerValue
+}
+
+// function calculateDealerValue(CFC) {
+//     tempValue = Value(CFC)
+//     if ((pCard1 == "AH" || pCard1 == "AD" || pCard1 == "AC" || pCard1 == "AS")) {
+//         if ((playerValue + 11) > 21) {
+//             pCard2_V = 1
+//         }
+//         else {
+//             pCard2_V = 11
+//         }
+//     }
+//     else {
+//         pCard1_V = Value(pCard1)
+//     }
+//     if ((pCard2 == "AH" || pCard2 == "AD" || pCard2 == "AC" || pCard2 == "AS")) {
+//         if ((playerValue + 11) > 21) {
+//             pCard2_V = 1
+//         }
+//         else {
+//             pCard2_V = 11
+//         }
+//     }
+//     else if ((hitNumber + 10) == 11) {
+//         pCard2_V = Value(pCard2)
+//     }
+//     if ((pCard3 == "AH" || pCard3 == "AD" || pCard3 == "AC" || pCard3 == "AS")) {
+//         if ((playerValue + 11) > 21) {
+//             pCard3_V = 1
+//         }
+//         else {
+//             pCard3_V = 11
+//         }
+//     }
+//     else if ((hitNumber + 10) == 12) {
+//         pCard3_V = Value(pCard3)
+//     }
+//     if ((pCard4 == "AH" || pCard4 == "AD" || pCard4 == "AC" || pCard4 == "AS")) {
+//         if ((playerValue + 11) > 21) {
+//             pCard4_V = 1
+//         }
+//         else {
+//             pCard4_V = 11
+//         }
+//     }
+//     else if ((hitNumber + 10) == 13) {
+//         pCard4_V = Value(pCard4)
+//     }
+//     if ((pCard5 == "AH" || pCard5 == "AD" || pCard5 == "AC" || pCard5 == "AS")) {
+//         if ((playerValue + 11) > 21) {
+//             pCard5_V = 1
+//         }
+//         else {
+//             pCard5_V = 11
+//         }
+//     }
+//     else if ((hitNumber + 10) == 14) {
+//         pCard5_V = Value(pCard5)
+//     }
+//     if ((pCard6 == "AH" || pCard6 == "AD" || pCard6 == "AC" || pCard6 == "AS")) {
+//         if ((playerValue + 11) > 21) {
+//             pCard6_V = 1
+//         }
+//         else {
+//             pCard6_V = 11
+//         }
+//     }
+//     else if ((hitNumber + 10) == 15) {
+//         pCard6_V = Value(pCard6)
+//     }
+//     if ((pCard7 == "AH" || pCard7 == "AD" || pCard7 == "AC" || pCard7 == "AS")) {
+//         if ((playerValue + 11) > 21) {
+//             pCard2_7 = 1
+//         }
+//         else {
+//             pCard2_7 = 11
+//         }
+//     }
+//     else if ((hitNumber + 10) == 16) {
+//         pCard7_V = Value(pCard7)
+//     }
+//     if ((pCard8 == "AH" || pCard8 == "AD" || pCard8 == "AC" || pCard8 == "AS")) {
+//         if ((playerValue + 11) > 21) {
+//             pCard8_V = 1
+//         }
+//         else {
+//             pCard8_V = 11
+//         }
+//     }
+//     else if ((hitNumber + 10) == 17) {
+//         pCard8_V = Value(pCard8)
+//     }
+//     if ((pCard9 == "AH" || pCard9 == "AD" || pCard9 == "AC" || pCard9 == "AS")) {
+//         if ((playerValue + 11) > 21) {
+//             pCard9_V = 1
+//         }
+//         else {
+//             pCard9_V = 11
+//         }
+//     }
+//     else if ((hitNumber + 10) == 18) {
+//         pCard9_V = Value(pCard9)
+//     }
+//     playerValue = pCard1_V + pCard2_V + pCard3_V + pCard4_V + pCard5_V + pCard6_V + pCard7_V + pCard8_V + pCard9_V
+//     console.log("playerValue = "+playerValue)
+//     document.getElementById("player-value").innerHTML = playerValue
+// }
